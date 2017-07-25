dda8defbdd10aa7c195251ea986c642a79e69f2d
“Vilde <vilde@vevatne.no>
2017-07-25 08:55:22 -0700

    Initial commit

---
+++ vandg/config/environment.rb
@@ -0,0 +1,36 @@
+# Load the Rails application.
+require_relative 'application'
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+
+# Initialize the Rails application.
+Rails.application.initialize!
